e13ef8068195bf4b913a13fee840edd361b9fb19
Giovanni Pizelli <giovannipizelli@gmail.com>
2018-06-12 12:04:26 -0300

    Projeto completo sem habilitar o tarefaConcluida, se habilitar não renderiza.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing-module';
+import { TarefasModule } from './tarefas/tarefas.module';
+//import { TarefaConcluidaDirective } from './tarefas/shared/tarefa-concluida.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent/*,
+    TarefaConcluidaDirective*/
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
